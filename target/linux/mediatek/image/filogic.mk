c5d95463ca55922e0bf7b7ff7fc6219075edad62
Matt Buczko <mbuczko@hotmail.com>
2026-04-25 12:26:10 -0500

    Update image configuration for wavlink_wl-wn586x3
    Removed factory.bin from images and updated sysupgrade.bin processing.
    
    Signed-off-by: Matt Buczko <mbuczko@hotmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -3138,11 +3138,10 @@ define Device/wavlink_wl-wn586x3
   DEVICE_DTS_LOADADDR := 0x47000000
   DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
   IMAGE_SIZE := 14336k
-  IMAGES := factory.bin sysupgrade.bin
+  IMAGES := sysupgrade.bin
   KERNEL := kernel-bin | lzma | \
 	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  IMAGE/factory.bin := $$(IMAGE/sysupgrade.bin)
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/sysupgrade.bin := append-kernel | pad-to 128k | append-rootfs | pad-rootfs | check-size | append-metadata
 endef
 TARGET_DEVICES += wavlink_wl-wn586x3
 
